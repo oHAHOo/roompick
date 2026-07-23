ed2e3496ccc8ea98abc8339de277393af2a5087c
Minjae <cmj011112@gmail.com>
2026-07-23 20:24:26 +0900

    feat: 마이그레이션 작성

---
+++ src/main/resources/db/migration/V4__create_payments.sql
@@ -0,0 +1,21 @@
+CREATE TABLE payments
+(
+    payment_id    BIGINT AUTO_INCREMENT NOT NULL,
+    reservation_id BIGINT                NOT NULL,
+    amount        BIGINT                 NOT NULL,
+    status        VARCHAR(20)            NOT NULL,
+    approved_at   DATETIME(6)            NULL,
+    failed_at     DATETIME(6)            NULL,
+    created_at    DATETIME(6)            NOT NULL,
+    updated_at    DATETIME(6)            NOT NULL,
+
+    CONSTRAINT pk_payments
+        PRIMARY KEY (payment_id),
+
+    CONSTRAINT uk_payments_reservation_id
+        UNIQUE (reservation_id),
+
+    CONSTRAINT fk_payments_reservation
+        FOREIGN KEY (reservation_id)
+            REFERENCES reservations (reservation_id)
+);
